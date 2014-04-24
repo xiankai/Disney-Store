71dc90b85cee1aa7adbcfeed0521d18a7c3e97aa
KJ <xiankai@gmail.com>
2014-04-24 22:23:25 +0800

    Proper working version

--- scheduler.php
+++ scheduler.php
@@ -3,62 +3,199 @@
 require 'vendor/autoload.php';
 
 $base_url = 'http://www.disneystore.com';
-
-$curl = new RollingCurl\RollingCurl();
 $client = new Predis\Client();
 
-$response = $curl
-	->get($base_url . '/disney/store/DSIProcessWidget?Nr=AND(pPublished%3A1%2CpBuyable%3A1)&Ne=1000201&categoryId=306000&storeId=10051&N=1021701+1000208&templateId=Width-3_4-ProductList&initialN=1021701&navNum=96');
-	->setCallback('parseStoreListing')
-	->execute();
+define('IN_STOCK', "1");
+define('OUT_OF_STOCK', "2");
+define('NO_STOCK', "3");
+define('UNKNOWN_ERROR', "4");
 
-function parseStoreList($request) {
-	$rolling_curl = new RollingCurl\RollingCurl();
+// Prime numbers
+define('NEW_ENTRY', "5");
+define('OLD_ENTRY', "7");
 
-	$items = json_decode($request->getResponseText());
-	foreach ($items->items as $item) {
-		// New item or out of stock
-		if (!$client->get($item->title)) {
-			$curl = new RollingCurl\Request('http://www.disneystore.com/disney/store/DSIAjaxOrderItemAdd', 'POST');
-			$curl->setExtraInfo($item);
-			$curl->setPostData(array(
-				'quantity' => 1,
-				'originalStoreId' => 10051,
-				'orderId' => '.',
-				'widget' => 'addToBag',
-				'catEntryId' => $item->productId,
-			));
+// Disney-specifc
+define('FROZEN', 1021701);
 
-			$rolling_curl->addRequest($curl);
-		} else {
-			updateRedis();
-		}
+function stringContains($haystack, $needle) {
+	return stripos($haystack, $needle) !== false;
+}
+
+$notify = function ($item, $stock) use ($base_url) {
+	$message = $item->title . ': ' . $base_url . $item->link . PHP_EOL;
+	$message .= 'Image: ' . $item->imageUrl . PHP_EOL;
+
+	switch ($item->status) {
+		case NEW_ENTRY: 
+			$message .= 'Status: ' . 'New';
+			$snippet = 'A new item ';
+			break;
+		case OLD_ENTRY: 
+			$message .= 'Status: ' . 'Old';
+			$snippet = 'An existing item ';
+			break;
 	}
 
-	$rolling_curl->setCallback(function($request, $rolling_curl) {
-		$item = $request->getExtraInfo();
+	$message .= PHP_EOL . 'Stock: ';
+	
+	switch ($stock) {
+		case IN_STOCK: 
+			$message .= 'Yes';
+			$snippet .= ' has been restocked!';
+			break;
+		case OUT_OF_STOCK: 
+			$message .= 'No';
+			$snippet .= ' has been listed but is out of stock.';
+			break;
+		case NO_STOCK: 
+			$message .= 'N/A';
+			$snippet .= ' has been listed!';
+			break;
+		case UNKNOWN_ERROR: 
+			$message .= 'Could not check';
+			$snippet .= ' has been listed but we could not check its stock.';
+			break;
+	}
 
-		// Curl for an item and request returns a positive
-		if ($item && stripos($request->getResponseText()) !== false) {
-			$message = $item->title . ': ' . $base_url . $item->link . PHP_EOL;
-			$message .= 'Image: ' . $item->imageUrl . PHP_EOL . PHP_EOL;
+	// Push notification curl onto the stack
+	$rolling_curl->post('https://api.pushover.net/1/messages.json', array(
+		'token' => 'aSruoKSByoBRHJfdx5ZTDZZEindFiE',
+		'user' => 'u4en9LeaFguiSD4gAewuRXkydRaKGw',
+		'message' => $message,
+	));
+};
 
-			// Push notification curl onto the stack
-			$rolling_curl->post('https://api.pushover.net/1/messages.json', array(
-				'token' => 'aSruoKSByoBRHJfdx5ZTDZZEindFiE',
-				'user' => 'u4en9LeaFguiSD4gAewuRXkydRaKGw',
-				'message' => $message,
-			));
+$setStore = function($key, $value) use ($client) {
+	$client->set($key, $value);
+};
 
-			updateRedis();
+$getStore = function($key) use ($client) {
+	return $client->get($key);
+};
+
+$keyExistsStore = function($key) use ($client) {
+	return !is_null($client->get($key));
+};
+
+$checkStock = function($request) use ($base_url, $notify, $setStore) {
+	$item = $request->getExtraInfo();
+	$message = $request->getResponseText();
+
+	if (stringContains($message, '_ERR_PROD_NOT_ORDERABLE')) {
+		$stock = OUT_OF_STOCK;
+	} elseif (stringContains($message, '_ERR_GETTING_SKU')) {
+		$stock = NO_STOCK;
+	} elseif (stringContains($message, '"catEntryId": "' . $item->productId . '"')) {
+		$stock = IN_STOCK;
+	} else {
+		$stock = UNKNOWN_ERROR;
+	}
+
+	if ($item->status === OLD_ENTRY && $stock === UNKNOWN_ERROR) {
+		// Don't update old entries with "error"
+	} else {
+		$setStore($item->title, $stock);
+	}
+
+	// Notify if new entry or if newly in stock.
+	if ($item->status === NEW_ENTRY 
+		|| $item->stock !== IN_STOCK && $stock === IN_STOCK
+		|| $item->stock === UNKNOWN_ERROR && $stock !== UNKNOWN_ERROR) {
+		$notify($item, $stock);
+	}
+};
+
+$parseStoreList = function ($request) use ($base_url, $checkStock, $notify, $getStore, $setStore, $keyExistsStore) {
+	$rolling_curl = new RollingCurl\RollingCurl();
+	$rolling_curl->setCallback($checkStock);
+
+	// Because the default of 5 makes the site choke and return more errors than usual. What.
+	$rolling_curl->setSimultaneousLimit(2);
+
+	// Split headers and body
+	$info = $request->getResponseInfo();
+	$header = substr($request->getResponseText(), 0, $info['header_size']);
+	$body = substr($request->getResponseText(), $info['header_size']);
+
+	// Get required cookies from header
+	$cookies = array();
+	$results = preg_match_all('/Set-Cookie: (.*?);/', $header, $cookies);
+	if (is_int($results) && $results > 0) {
+		$cookies = implode('; ', $cookies[1]) . ';';
+	} else {
+		echo $request->getResponseText();
+		throw new DisneyException('Unable to get required cookie');
+	}
+
+	$items = json_decode($body);
+
+	if (is_null($items)) {
+		// This is as 'items'  key is not present, so there is a trailing comma in the JSON string which renders it invalid.
+		// Great job, Disney.
+		throw new DisneyException('No items found.'); 
+	}
+
+	foreach ($items->items as $item) {
+		// Some store items have same exact names. Use product ID to distinguish.
+		$item->title .= " ({$item->productId})";
+		$item->status = $keyExistsStore($item->title) ? OLD_ENTRY : NEW_ENTRY;
+		$item->stock = $getStore($item->title);
+
+		if (
+			$item->status === NEW_ENTRY && (
+				// Collections are basically groups of items that may or may not be elsewhere in the listings.
+				$item->isCollection === "true" ||
+				// Customizable items cannot have stock tracked	
+				stringContains($item->title, 'Create Your Own')
+			)
+		) {
+			$setStore($item->title, NO_STOCK);
+			$notify($item, NO_STOCK);
+			continue;
 		}
-	});
-}
 
-function updateRedis() {
-	// value isn't important
-	$client->set($item->title, 1);
+		// Refresh timer
+		if ($item->stock === NO_STOCK) {
+			$setStore($item->title, NO_STOCK);
+			continue;
+		}
 
-	// Expire in one day, to account for possible downtime.
-	$client->expire($item->title, 3600 * 24);
-}
+		// Check for stock
+		$curl = new RollingCurl\Request($base_url . '/disney/store/DSIAjaxOrderItemAdd', 'POST');
+
+		// Set required cookies
+		$curl->addOptions(array(
+			CURLOPT_COOKIESESSION => true,
+			CURLOPT_COOKIE => $cookies,
+		))
+		->setExtraInfo($item)
+		->setPostData(array(
+			'quantity' => 1,
+			'originalStoreId' => 10051,
+			'catEntryId' => $item->productId,
+		));
+
+		$rolling_curl->add($curl);
+	}
+
+	$rolling_curl->execute();
+};
+
+$params = http_build_query(array(
+	'storeId' => 10051,
+	'N' => FROZEN,
+	'templateId' => 'Width-3_4-ProductList',
+	'navNum' => 96,
+));
+
+$curl = new RollingCurl\RollingCurl();
+$response = $curl
+	->get($base_url . '/disney/store/DSIProcessWidget?' . $params)
+	->addOptions(array(
+		CURLOPT_HEADER => true,
+		CURLOPT_COOKIESESSION => true,
+	))
+	->setCallback($parseStoreList)
+	->execute();
+
+class DisneyException extends Exception {}
